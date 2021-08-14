ac6a4649e3dcb7ea5acf9726417967aaf948f1ef
Lachlan Daly <l.daly@uqconnect.edu.au>
2021-08-14 16:00:40 +1000

    created README for project and displayed deterministic message in project

---
+++ README.md
@@ -0,0 +1,32 @@
+# Mixed Messages - A Codecademy project
+Simple program created with Node.js
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Features](#features)
+* [Sources](#sources)
+
+## General info
+This project is a simple message generator. Still working on what kind of message this will send
+
+## Technologies
+Project is created with:
+* Node version: 14.17.5
+
+## Setup
+To run this project, 
+```
+$ node ./script.js
+```
+
+## Features
+* Have fun generating randomised messages that may or may not make any sense
+
+### To Do:
+* Choose a topic for the messages
+* Use script within a larger project
+
+## Sources
+This app was inspired by Codecademy who gave the project description
--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log('working?');
+// Author: Lachlan Daly
+// Date: 14/08/2021
+
+
+// Function to generate the first randomised line of the message
+generateFirstLine = () => {
+    return 'first line';
+}
+
+
+// Function to generate the second randomised line of the message
+generateSecondLine = () => {
+    return 'second line';
+}
+
+
+// Function to generate the third randomised line of the message
+generateThirdLine = () => {
+    return 'third line';
+}
+
+
+// Function to generate a random integer between min and max
+generateRandomNumber = (min, max) => {
+    return 1;
+}
+
+
+// This function uses helper functions to grab and display
+// all three randomised sentences.
+displayMessage = () => {
+    const firstMessage = generateFirstLine();
+    const secondMessage = generateSecondLine();
+    const thirdMessage = generateThirdLine();
+    const completeMessage = `${firstMessage} ${secondMessage} ${thirdMessage}`
+    console.log(completeMessage);
+}
+
+
+displayMessage();
